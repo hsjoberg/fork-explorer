36bfd907f6d26d6bd2389a491891030ca68dea5a
Ben Carman <benthecarman@live.com>
2021-05-02 02:34:07 -0500

    Add twitter link preview

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -2,6 +2,9 @@ import React, { ComponentType, useEffect, useState } from "https://esm.sh/react@
 import { StoreProvider } from "https://esm.sh/easy-peasy";
 
 import store, { useStoreActions } from "./state/index.ts";
+import config from "../back/config/config.ts";
+
+const forkName = config.fork.name;
 
 function StoreStarter({ Page, pageProps }: { Page: ComponentType<any>; pageProps: any }) {
   const [gotBlocks, setGotBlocks] = useState(false);
@@ -29,6 +32,15 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>; pag
         <meta name="viewport" content="width=device-width" />
         <link rel="stylesheet" href="./style/site.css" />
         <link rel="stylesheet" href="./style/reset.css" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@hampus_s" />
+        <meta name="twitter:creator" content="@hampus_s" />
+        <meta name="twitter:image" content="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/54056/carrot-emoji-clipart-md.png" />
+        <meta property="og:image" content="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/54056/carrot-emoji-clipart-md.png" />
+        <meta property="og:url" content="https://taproot.watch" />
+        <meta property="og:title"><content>{forkName} Activation</content></meta>
+        <meta property="og:description"><content>See the current signalling status of the {forkName} softfork.</content></meta>
       </head>
       <a href="https://github.com/hsjoberg/fork-explorer" className="github-corner" aria-label="View source on GitHub">
         <svg width="80" height="80" viewBox="0 0 250 250" className="github-corner-svg" aria-hidden="true">
