2939fcf589611ec94cb487851cb5089bd9ea18fc
tomat <tomat@blarg.se>
2021-05-04 11:06:02 +0200

    show current total on miners page

--- txt/index.ts
+++ txt/index.ts
@@ -24,6 +24,10 @@ export function homeTXT() {
   const miners = computeMiners(blocks);
   const forkName = config.fork.name;
 
+  const totalSignalling = miners
+    .filter(([_, m]) => m.signals)
+    .reduce((sum, [_, m]) => sum + m.numBlocks / currentNumberOfBlocks, 0);
+
   let blocksTable = "";
   blocksTable += `${blocks[0].height}`;
   for (let i = 0; i < blocks.length; i++) {
@@ -88,6 +92,8 @@ ${blocksTable}
 ---
 
 Miners
+
+Current total: ${(totalSignalling * 100).toFixed(2)}% ✅
 
 ${miners
   .map(
