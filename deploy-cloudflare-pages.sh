08bee715124568b0e8d13aa68a6c6f644c74caa5
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-05-06 17:36:19 +0200

    Fix for CloudFlare Pages

--- deploy-cloudflare-pages.sh
+++ deploy-cloudflare-pages.sh
@@ -8,4 +8,7 @@ unzip deno-x86_64-unknown-linux-gnu.zip
 cp config/config.ts_TEMPLATE config/config.ts
 sed -i 's/mode: "real"/mode: "fake-frontend"/g' config/config.ts
 
+
+# Build
+mkdir frontend/dist
 ./deno run -A https://deno.land/x/aleph@v0.3.0-alpha.32/cli.ts build frontend --reload
