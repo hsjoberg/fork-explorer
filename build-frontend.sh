3306e98dcccbbc48dab1abda4f02c041a539873d
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-05-06 17:44:04 +0200

    Fix scripts

--- build-frontend.sh
+++ build-frontend.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir frontend/dist 2>/dev/null
 deno run -A https://deno.land/x/aleph@v0.3.0-alpha.32/cli.ts build frontend --reload
--- clean.sh
+++ clean.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 rm -r frontend/dist 2>/dev/null
-mkdir frontend/dist
