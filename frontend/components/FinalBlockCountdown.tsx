ece3d0be3e57588aa4396d0166beb24d8fef2259
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-05-30 20:03:02 +0200

    index: Add final block countdown when less than 67 signalling blocks left

---
+++ frontend/components/FinalBlockCountdown.tsx
@@ -0,0 +1,46 @@
+import React from "https://esm.sh/react@17.0.2";
+import styled from "https://esm.sh/styled-components";
+
+import { BlockContainer, Block, EmptyBlock } from "./Block.tsx";
+import CommonHeader from "./CommonHeader.ts";
+import { useStoreState } from "../state/index.ts";
+import config from "../back/config/config.ts";
+import { computeStats } from "../back/common/data.ts";
+
+const FinalCountdownHolder = styled.div`
+  margin: auto;
+`;
+
+const FinalCountdownBar = styled(BlockContainer)``;
+
+export default function FinalBlockCountdown() {
+  const blocks = useStoreState((store) => store.blocks);
+  const forkName = config.fork.name;
+  const { blocksLeftForActivation } = computeStats(blocks);
+
+  if (blocksLeftForActivation === 0) {
+    return null;
+  }
+
+  const emptyBlocks = new Array(blocksLeftForActivation).fill(<EmptyBlock big nextBlock={false} />);
+  const r = blocks
+    .filter((block) => block.signals)
+    .slice(0 - (33 * 2 - blocksLeftForActivation))
+    .map((block, i) => {
+      if (block.signals === undefined) {
+        return;
+      }
+      return <Block big key={i} height={block.height} signals={block.signals} miner={block.miner} selected={false} />;
+    })
+    .concat(emptyBlocks);
+
+  return (
+    <FinalCountdownHolder>
+      <CommonHeader>
+        {blocksLeftForActivation} more signalling block{blocksLeftForActivation > 1 && "s"} required for the {forkName}{" "}
+        softfork to lock in!
+      </CommonHeader>
+      <FinalCountdownBar>{r}</FinalCountdownBar>
+    </FinalCountdownHolder>
+  );
+}
