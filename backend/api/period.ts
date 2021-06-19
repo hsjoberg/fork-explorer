1d0e7357c3ec5ddb7a82a8550347aa38f1a26620
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-19 04:21:38 -0300

    Update period.ts

--- backend/api/period.ts
+++ backend/api/period.ts
@@ -1,24 +1,24 @@
-import { RouterMiddleware } from "https://deno.land/x/oak@v7.5.0/mod.ts";
-import { exists } from "https://deno.land/std@0.98.0/fs/mod.ts";
+ { RouterMiddleware }  "https://deno.land/x/oak@v7.5.0/mod.ts";
+ { exists }  "https://deno.land/std@0.98.0/fs/mod.ts";
 
-export const GetPeriod: RouterMiddleware = async (context) => {
-  try {
-    const period = Number.parseInt(context.params.period || "");
-    if (Number.isNaN(period)) {
+ GetPeriod: RouterMiddleware =  (context) => {
+   {
+     period = Number.parseInt(context.params.period || "");
+      (Number.isNaN(period)) {
       context.response.status = 404;
       context.response.body = "404 File Not Found";
-      return;
+      ;
     }
 
-    const path = Deno.cwd() + `/data/periods/${context.params.period}.json`;
-    if (!(await exists(path))) {
+     path = Deno.cwd() + `/data/periods/${context.params.period}.json`;
+     (!(await exists(path))) {
       context.response.status = 404;
       context.response.body = "404 File Not Found";
-      return;
+      ;
     }
 
-    const decoder = new TextDecoder("utf-8");
-    const blocks = await Deno.readFile(path);
+     decoder =  TextDecoder("utf-8");
+      blocks =  Deno.readFile(path);
     context.response.body = JSON.parse(decoder.decode(blocks));
   } catch (error) {
     console.log(error);
