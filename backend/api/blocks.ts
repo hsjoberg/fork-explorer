b5e57c6afb7b614e20a0fef110ca96bd59085cfc
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-19 04:18:36 -0300

    Update blocks.ts

--- backend/api/blocks.ts
+++ backend/api/blocks.ts
@@ -1,8 +1,8 @@
-import { RouterMiddleware } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+ { RouterMiddleware }  "https://deno.land/x/oak@v7.5.0/mod.ts";
 
-import { getBlocks } from "../blocks/index.ts";
+ { getBlocks }  "../blocks/index.ts";
 
-export const GetBlocks: RouterMiddleware = (context) => {
-  const blocks = getBlocks();
+ GetBlocks: RouterMiddleware = (context) => {
+   blocks = getBlocks();
   context.response.body = blocks;
 };
