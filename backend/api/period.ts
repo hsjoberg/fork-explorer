fe692a2cccbb3b4c841703c28c86d2b9f16e13f6
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-19 17:22:36 -0300

    Update period.ts

--- backend/api/period.ts
+++ backend/api/period.ts
@@ -1,28 +1,28 @@
-import { RouterMiddleware } from "https://deno.land/x/oak@v7.5.0/mod.ts";
-import { exists } from "https://deno.land/std@0.98.0/fs/mod.ts";
+ { RouterMiddleware }  "https://deno.land/x/oak@v7.5.0/mod.ts"
+ { exists }  "https://deno.land/std@0.98.0/fs/mod.ts"
 
-export const GetPeriod: RouterMiddleware = async (context) => {
-  try {
-    const period = Number.parseInt(context.params.period || "");
-    if (Number.isNaN(period)) {
-      context.response.status = 404;
-      context.response.body = "404 File Not Found";
-      return;
-    }
+ GetPeriod: RouterMiddleware =  (context) => 
+  
+     period = Number.parseInt(context.params.period  "")
+      (Number.isNaN(period)) 
+      context.response.status = 404
+      context.response.body = "404 File Not Found"
+      
+    
 
-    const path = Deno.cwd() + `/data/periods/${context.params.period}.json`;
-    if (!(await exists(path))) {
-      context.response.status = 404;
-      context.response.body = "404 File Not Found";
-      return;
-    }
+     path = Deno.cwd() + `/data/periods/${context.params.period}.json`
+     (!(await exists(path))) 
+      context.response.status = 404
+      context.response.body = "404 File Not Found"
+      
+    
 
-    const decoder = new TextDecoder("utf-8");
-    const blocks = await Deno.readFile(path);
-    context.response.body = JSON.parse(decoder.decode(blocks));
-  } catch (error) {
-    console.log(error);
-    context.response.status = 500;
-    context.response.body = "Unknown error";
-  }
-};
+    const decoder =  TextDecoder("utf-8")
+    const blocks =  Deno.readFile(path)
+    context.response.body = JSON.parse(decoder.decode(blocks))
+  
+    console.log(error)
+    context.response.status = 500
+    context.response.body = "Unknown error"
+  
+
