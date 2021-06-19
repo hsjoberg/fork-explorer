619e59ad57319dde01791b084ea871775f1dbac2
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-19 04:23:50 -0300

    Update misc.ts

--- backend/api/misc.ts
+++ backend/api/misc.ts
@@ -1,28 +1,28 @@
-import { RouterMiddleware } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+ { RouterMiddleware }  "https://deno.land/x/oak@v7.5.0/mod.ts";
 
-import { getblockhash } from "../jsonrpc/index.ts";
-import { getblockchaininfo, getblockcount } from "../jsonrpc/index.ts";
+ { getblockhash }  "../jsonrpc/index.ts";
+ { getblockchaininfo, getblockcount }  "../jsonrpc/index.ts";
 
-export const GetBlockchainInfo: RouterMiddleware = async (context) => {
-  const info = await getblockchaininfo();
+ GetBlockchainInfo: RouterMiddleware =  (context) => {
+   info =  getblockchaininfo();
   console.log(info);
   context.response.body = info;
 };
 
-export const GetBlockHash: RouterMiddleware<{ height: string }> = async (context) => {
-  if (!context.params || !context.params.height) {
+ GetBlockHash: RouterMiddleware<{ height: string }> =  (context) => {
+   (!context.params || !context.params.height) {
     context.response.status = 400;
     context.response.body = JSON.stringify({
       status: "ERROR",
       reason: "Missing param height",
     });
-    return;
+    ;
   }
-  const blockhash = await getblockhash(Number.parseInt(context.params.height));
+   blockhash =  getblockhash(Number.parseInt(context.params.height));
   context.response.body = blockhash;
 };
 
-export const GetBlockCount: RouterMiddleware = async (context) => {
-  const blockCount = await getblockcount();
+ GetBlockCount: RouterMiddleware =  (context) => {
+   blockCount =  getblockcount();
   context.response.body = blockCount;
 };
