007c1164e3f507e37a590bbdceed078f6f7616e1
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-19 17:18:34 -0300

    Update index.ts

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,24 +1,23 @@
-import { Router } from "https://deno.land/x/oak@v7.5.0/mod.ts";
+ { Router }  "https://deno.land/x/oak@v7.5.0/mod.ts"
 
-import { GetBlocks } from "./blocks.ts";
-import { GetPeriod } from "./period.ts";
-import { LnurlPayRequest, LnurlPayRequestCallback } from "./donate.ts";
-import { GetBlockchainInfo, GetBlockCount, GetBlockHash } from "./misc.ts";
+ { GetBlocks }  "./blocks.ts"
+ { GetPeriod }  "./period.ts"
+ { LnurlPayRequest, LnurlPayRequestCallback }  "./donate.ts"
+ { GetBlockchainInfo, GetBlockCount, GetBlockHash }  "./misc.ts"
 
-const router = new Router();
+ router =  Router()
 
-router.get("/blocks", GetBlocks);
+router.get("/blocks", GetBlocks)
 
-router.get("/period/:period", GetPeriod);
+router.get("/period/:period", GetPeriod)
 
-router.get("/invoice", LnurlPayRequest);
+router.get("/invoice", LnurlPayRequest)
 
-router.get("/invoice/callback", LnurlPayRequestCallback);
+router.get("/invoice/callback", LnurlPayRequestCallback)
 
-router.get("/getblockchaininfo", GetBlockchainInfo);
+router.get("/getblockchaininfo", GetBlockchainInfo)
 
-router.get<{ height: string }>("/getblockhash/:height", GetBlockHash);
+router.get<{ height: string }>("/getblockhash/:height", GetBlockHash)
+router.get("/getblockcount", GetBlockCount)
 
-router.get("/getblockcount", GetBlockCount);
-
-export default router;
+ router
